4cdb133b0c2f0357c0af640a782427a8c0e80728
Tyler Young <tyler@x-plane.com>
2019-07-30 14:49:34 -0500

    feat (files): Added MaybePathlike

--- files.py
+++ files.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Union, Iterable
 
 Pathlike = Union[Path, str]
+MaybePathlike = Union[Pathlike, None]
 
 def dir_size(directory: Path) -> int: return file_sizes(f for f in directory.iterdir() if f.is_file())
 def file_size(f: Path) -> int: return os.path.getsize(str(f))
