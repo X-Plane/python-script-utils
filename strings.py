e4aa10e25438a11a460d2050655857fb28c5d398
Tyler Young <tyler@x-plane.com>
2019-08-01 11:47:22 -0500

    feat (strings): Added print_all()

--- strings.py
+++ strings.py
@@ -1,3 +1,9 @@
+from typing import Iterable, Any
+
 
 def quoted(stringlike):
     return '"%s"' % stringlike
+
+def print_all(stringlikes: Iterable[Any], prefix='', suffix='\n'):
+    for s in stringlikes:
+        print('%s%s' % (prefix, s), end=suffix)
